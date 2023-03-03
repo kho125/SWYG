5595efcfa9183fe2354b9d4f574ce4f130b9abcd
kho125 <kyeongho94@gmail.com>
2023-03-03 15:17:56 +0900

    UI

--- src/components/units/narrationLast/NarrationLast2/NarrationLast2.presenter.jsx
+++ src/components/units/narrationLast/NarrationLast2/NarrationLast2.presenter.jsx
@@ -5,12 +5,12 @@ export default function NarrationStart2UI(props) {
   return (
     <>
       <S.Wrapper>
-      <S.Bg>
-        <S.NarrationBox>
-          <p>메신저가 하나 와있다.</p>
-          <p>메신저를 보낸 사람은,,,,,?</p>
-        </S.NarrationBox>
-        <S.StartBtn onClick={props.onClickMove}>계속</S.StartBtn>
+        <S.Bg>
+          <S.NarrationBox>
+            <p>메신저가 하나 와있다.</p>
+            <p>메신저를 보낸 사람은,,,,,?</p>
+          </S.NarrationBox>
+          <S.StartBtn onClick={props.onClickMove}>계속</S.StartBtn>
         </S.Bg>
         <S.Bottom></S.Bottom>
       </S.Wrapper>
